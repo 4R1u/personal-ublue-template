5b214e438bc61c831f8bb320ec6b3c9c81759ad7
4R1u <62248461+4R1u@users.noreply.github.com>
2026-07-21 16:23:19 +0500

    feat: add niri and dms command

--- build_files/build.sh
+++ build_files/build.sh
@@ -21,6 +21,10 @@ dnf5 install -y tmux
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+dnf copr enable avengemedia/dms
+dnf install niri dms
+systemctl add-wants niri.service dms
+dnf copr disable avengemedia/dms
 
 #### Example for enabling a System Unit File
 
