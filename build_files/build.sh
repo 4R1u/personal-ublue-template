dc34a012290e26588b347625a3f3ccae135a11cb
4R1u <62248461+4R1u@users.noreply.github.com>
2026-07-22 09:17:25 +0500

    feat: add potentially missing stuff

--- build_files/build.sh
+++ build_files/build.sh
@@ -21,10 +21,15 @@ dnf5 install -y tmux
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
- dnf5 copr enable avengemedia/dms
- dnf5 install -y niri dms
-# systemctl --user add-wants niri.service dms
+dnf5 copr enable avengemedia/dms
+dnf5 copr enable avengemedia/danklinux
+dnf copr enable scottames/ghostty
+dnf5 install -y niri dms dms-greeter dankcalendar-git cava ghostty
+dms setup
+systemctl --user add-wants niri.service dms
 # dnf5 copr disable avengemedia/dms
+# dnf5 copr disable avengemedia/danklinux
+# dnf5 copr disable scottames/ghostty
 
 # headless install command from dms website
 # sudo -v
